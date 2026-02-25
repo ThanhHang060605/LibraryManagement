66b2a1ff2df16ddf5603f179084785607f829bf2
ThanhHang060605 <phamthanhhangqp66@gmai.com>
2026-02-25 19:16:21 +0700

    Initial project structure

---
+++ LibraryManagement/UI/LoginWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using LibraryManagement.Models;
+
+namespace LibraryManagement.UI
+{
+    public partial class LoginWindow : Window
+    {
+        private List<Account> accounts;
+
+        public LoginWindow()
+        {
+            InitializeComponent();
+            LoadAccounts();
+        }
+
+        private void LoadAccounts()
+        {
+            accounts = new List<Account>
+            {
+                new Account { Username = "admin", Password = "12345", Role = "Admin" },
+                new Account { Username = "user", Password = "12345", Role = "User" }
+            };
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            string username = txtUsername.Text;
+            string password = txtPassword.Password;
+
+            var account = accounts
+                .FirstOrDefault(a => a.Username == username && a.Password == password);
+
+            if (account != null)
+            {
+                MessageBox.Show("Login successful!");
+
+                MainWindow main = new MainWindow(account.Role);
+                main.Show();
+
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Invalid username or password!");
+            }
+        }
+    }
+}
---
+++ LibraryManagement/UI/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LibraryManagement.UI
+{
+    public partial class MainWindow : Window
+    {
+        private string role;
+
+        public MainWindow(string role)
+        {
+            InitializeComponent();
+            this.role = role;
+            SetupRole();
+        }
+
+        private void SetupRole()
+        {
+            txtRole.Text = "Logged in as: " + role;
+
+            if (role == "User")
+            {
+                btnManageBooks.Visibility = Visibility.Collapsed;
+                btnDashboard.Visibility = Visibility.Collapsed;
+            }
+        }
+    }
+}
